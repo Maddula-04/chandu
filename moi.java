28908c4b03b3d7165f279c5bb638c7f923a1d2c4
Maddula-04 <121088504+Maddula-04@users.noreply.github.com>
2022-12-20 13:57:02 +0530

    Add files via upload

---
+++ moi.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class moi
+{
+   public static void main(string args[])
+   {
+       int num=5;
+       for(i=1,i<=10,i++)
+       {
+          System.out.println("table"+(i*n));
+       }
+    }
+} 
